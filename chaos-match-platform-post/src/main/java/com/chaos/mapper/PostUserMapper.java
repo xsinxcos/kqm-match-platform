cfca2c0e02c4e760d18f4eecec32c9b94780f740
xsinxcos <1465701009@qq.com>
2024-02-04 03:57:16 +0800

    feat:新增用户与帖子匹配的关系类

---
+++ chaos-match-platform-post/src/main/java/com/chaos/mapper/PostUserMapper.java
@@ -0,0 +1,15 @@
+package com.chaos.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.chaos.entity.PostUser;
+
+
+/**
+ * 帖子与用户匹配成功关系表(PostUser)表数据库访问层
+ *
+ * @author chaos
+ * @since 2024-02-04 03:55:28
+ */
+public interface PostUserMapper extends BaseMapper<PostUser> {
+
+}
---
+++ chaos-match-platform-post/src/main/java/com/chaos/service/PostUserService.java
@@ -0,0 +1,16 @@
+package com.chaos.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.chaos.entity.PostUser;
+
+
+/**
+ * 帖子与用户匹配成功关系表(PostUser)表服务接口
+ *
+ * @author chaos
+ * @since 2024-02-04 03:55:30
+ */
+public interface PostUserService extends IService<PostUser> {
+
+}
+
